6004412d8c3bb2d98d2a7a9d468c91f761324d8e
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-05-03 23:10:34 -0300

    Implementando a pagina contato e iniciando a pagina home

--- siteCresci/src/app/contato/contato.component.ts
+++ siteCresci/src/app/contato/contato.component.ts
@@ -9,7 +9,32 @@ export class ContatoComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): any{
+    (function() {
+      "use strict";
+      window.addEventListener(
+        "load",
+        function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName("needs-validation");
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener(
+              "submit",
+              function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+              },
+              false
+            );
+          });
+        },
+        false
+      );
+    })();
   }
 
 }
