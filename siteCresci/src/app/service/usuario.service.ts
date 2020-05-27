e71c310b1a9b14290af58a41c52a4ae6600158ae
Bielzin42 <nickspoot@gmail.com>
2020-05-27 09:51:13 -0300

    uploading putCadastro branch

--- siteCresci/src/app/service/usuario.service.ts
+++ siteCresci/src/app/service/usuario.service.ts
@@ -10,10 +10,18 @@ export class UsuarioService {
   constructor(private http: HttpClient) { }
 
   getAllUsuarios() {
-    return this.http.get('http://93.188.161.223:9000/user')
+    return this.http.get('http://93.188.161.223:9000/user');
   }
 
   postUsuario(usuario: Usuario) {
-    return this.http.post('http://93.188.161.223:9000/user', usuario)
+    return this.http.post('http://93.188.161.223:9000/user', usuario);
+  }
+
+  putUsuario(usuario: Usuario){
+    return this.http.put('http://93.188.161.223:9000/user', usuario);
+  }
+
+  getByIdUsuario(id: number){
+    return this.http.get(`http://93.188.161.223:9000/user${id}`)
   }
 }
