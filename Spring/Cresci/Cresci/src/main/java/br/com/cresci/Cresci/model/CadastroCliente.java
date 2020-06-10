e5484ad9810307528cb1b41a863039b8f7be6b29
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-06-10 14:32:06 -0300

    Implemetando model Transação e package Repository

--- Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/model/CadastroCliente.java
+++ Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/model/CadastroCliente.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -33,7 +35,9 @@ public class CadastroCliente {
 	@Size(min = 8, max = 30)
 	private String senha;
 	
-	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private EnderecoCliente enderecoCliente;
 
 	public long getId() {
 		return id;
@@ -74,5 +78,15 @@ public class CadastroCliente {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public EnderecoCliente getEnderecoCliente() {
+		return enderecoCliente;
+	}
+
+	public void setEnderecoCliente(EnderecoCliente enderecoCliente) {
+		this.enderecoCliente = enderecoCliente;
+	}
+
+	
 
 }
---
+++ Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/model/TransacaoClienteProduto.java
@@ -0,0 +1,29 @@
+package br.com.cresci.Cresci.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.OneToMany;
+//import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table (name = "transacao")
+public class TransacaoClienteProduto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+			
+	//@OneToOne
+	//@JoinColumn(nullable = false)
+	//private Produto produto;
+	
+	//@OneToMany
+	//@JoinColumn(nullable = false)
+	//private CadastroCliente cadastroCliente;
+	
+}
