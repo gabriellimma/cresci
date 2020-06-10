2c2f5607053589f478db4dc0fccd853c88fa0310
Erick005 <erickportugaldesenvolvedor@gmail.com>
2020-06-10 15:05:41 -0300

    Implementado repository

---
+++ Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/repository/EnderecoClienteRepository.java
@@ -0,0 +1,9 @@
+package br.com.cresci.Cresci.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.cresci.Cresci.model.EnderecoCliente;
+
+public interface EnderecoClienteRepository extends JpaRepository<EnderecoCliente, Long>{
+
+}
