c71e723c27f664f5ea1652e4dcd624eea434f355
Bielzin42 <nickspoot@gmail.com>
2020-06-10 19:47:19 -0300

    adding cadastroCliente endpoints

--- Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/controller/CadastroClienteController.java
+++ Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/controller/CadastroClienteController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +40,15 @@ public class CadastroClienteController {
 	@PostMapping
 	public ResponseEntity<CadastroCliente> Post(@RequestBody CadastroCliente cadastroCliente){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(cadastroCliente));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CadastroCliente> Put(@RequestBody CadastroCliente cadastro){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(cadastro));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
