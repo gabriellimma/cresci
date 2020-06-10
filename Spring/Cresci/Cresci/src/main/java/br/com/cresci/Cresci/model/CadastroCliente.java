8226ff6cad3e77c862b50578cfbf6b58b2891d3c
Ivy-Silva <ivyrebeca@gmail.com>
2020-06-10 11:41:18 -0300

    Implementando model cliente Spring

---
+++ Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/model/CadastroCliente.java
@@ -0,0 +1,78 @@
+package br.com.cresci.Cresci.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "cliente")
+public class CadastroCliente {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nomeCliente;
+	
+	@NotNull
+	@Size(min = 11, max = 11) 
+	private String cpf;
+	
+	@NotNull
+	@Size(min = 8, max = 50)
+	private String email;
+	
+	@NotNull
+	@Size(min = 8, max = 30)
+	private String senha;
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
