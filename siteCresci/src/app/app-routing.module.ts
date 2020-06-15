097a10b2e976113c188329e791c32304b9712e6a
Ivy-Silva <ivyrebeca@gmail.com>
2020-06-14 22:32:00 -0300

    implementando o componente Termos de Uso

--- siteCresci/src/app/app-routing.module.ts
+++ siteCresci/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { PrivacidadeComponent } from './privacidade/privacidade.component';
 import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
 import { DeletarUsuarioComponent } from './deletar-usuario/deletar-usuario.component';
 import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { TermosDeUsoComponent } from './termos-de-uso/termos-de-uso.component';
 
 
 const routes: Routes = [
@@ -29,7 +30,8 @@ const routes: Routes = [
   { path: 'privacidade', component: PrivacidadeComponent },
   { path: 'editar-usuario/:id', component: EditarUsuarioComponent },
   { path: 'deletar-usuario/:id', component: DeletarUsuarioComponent },
-  { path: 'cadastro-produto', component: CadastroProdutoComponent}
+  { path: 'cadastro-produto', component: CadastroProdutoComponent},
+  { path: 'termos-de-uso', component: TermosDeUsoComponent }
 ];
 
 @NgModule({
