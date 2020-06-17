e49e6882aa7372c1ac902f29283e435422d3db28
Bielzin42 <nickspoot@gmail.com>
2020-06-17 17:00:46 -0300

    deletando data base

--- Spring/Cresci/Cresci/src/main/java/br/com/cresci/Cresci/repository/CadastroClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cresci.Cresci.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.cresci.Cresci.model.CadastroCliente;
-
-public interface CadastroClienteRepository extends JpaRepository<CadastroCliente, Long> {
-
-}
