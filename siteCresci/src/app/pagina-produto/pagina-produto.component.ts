a7bc8fa530609f44b54ad3f51fdde34291a5e594
GeisianeMaia <geisiane.maia@gmail.com>
2020-06-18 13:58:48 -0300

    Implementado a interpolação

--- siteCresci/src/app/pagina-produto/pagina-produto.component.ts
+++ siteCresci/src/app/pagina-produto/pagina-produto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../model/Usuario';
+import { ProdutoService } from '../service/produto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-produto',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaProdutoComponent implements OnInit {
 
-  constructor() { }
+  produto:Produto = new Produto
+  // postagem:Postagem = new Postagem
+  constructor( private produtoService: ProdutoService, private route: ActivatedRoute, private router: Router) { }
+  // private postagemService: PostagemService, private route: ActivatedRoute, private router: Router
 
   ngOnInit(): void {
+    let id:number  = this.route.snapshot.params['id']
+    this.findById(id)
   }
 
+  findById(id:number){
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto)=>{
+      this.produto = resp
+    }, err => {
+      console.log(`Erro: ${err.status}, não conseguimos pegar o id`)
+    });
+  }
+
+  // findallProdutos(){
+  //   this.UsuarioService.getAllUsuarios().subscribe((resp: Produto[])=>{
+  //     this.listaProdutos = resp
+  //   })
+  // }
 }
