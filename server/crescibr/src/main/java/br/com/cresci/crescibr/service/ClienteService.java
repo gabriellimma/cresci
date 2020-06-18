6c89ff88690fb008818b40318d8c2d0dc08b6d56
Bielzin42 <nickspoot@gmail.com>
2020-06-18 14:29:01 -0300

    criando clienteService e auntenticação de email e senha e toking basic

--- server/crescibr/src/main/java/br/com/cresci/crescibr/service/ClienteService.java
+++ server/crescibr/src/main/java/br/com/cresci/crescibr/service/ClienteService.java
@@ -1,9 +1,14 @@
 package br.com.cresci.crescibr.service;
 
+import java.nio.charset.Charset;
+import org.apache.commons.codec.binary.Base64;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.cresci.crescibr.model.ClienteLoginModel;
 import br.com.cresci.crescibr.model.ClienteModel;
 import br.com.cresci.crescibr.repository.ClienteRepository;
 
@@ -21,4 +26,25 @@ public class ClienteService {
 		usuario.setSenha(senhaEncoder);
 		return repository.save(usuario);
 	}
+	
+	public Optional<ClienteLoginModel> Logar(Optional<ClienteLoginModel> user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<ClienteModel> usuario = repository.findByEmail(user.get().getEmail());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				
+				String auth =  user.get().getEmail() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				
+				String authHeader = "Basic " + new String(encodedAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setEmail(usuario.get().getEmail());
+				
+				return user;
+			}
+		}
+		return null;
+	}	
 }
