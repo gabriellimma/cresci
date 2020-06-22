afe1ecde043c76b3c8d8ae82765ab171f1791578
Bielzin42 <nickspoot@gmail.com>
2020-06-22 13:27:48 -0300

    criação dos endpoins de produto service

--- siteCresci/src/app/service/produto.service.ts
+++ siteCresci/src/app/service/produto.service.ts
@@ -13,11 +13,31 @@ export class ProdutoService {
     return this.http.get('http://localhost:8080/produtos');
   }
 
-  postProduto(usuario: Produto) {
-    return this.http.post('http://93.188.161.223:9000/user', usuario);
+  postProduto(produto: Produto) {
+    return this.http.post('http://93.188.161.223:9000/user', produto);
   }
 
   getByIdProduto(id: number){
-    return this.http.get(`http://93.188.161.223:9000/user/${id}`);
+    return this.http.get(`http://localhost:8080/produtos/${id}`);
+  }
+
+  getByCategoriaProduto(categoria: string){
+    return this.http.get(`http://localhost:8080/produtos/categoria/${categoria}`);
+  }
+
+  getByTituloProduto(titulo: string){
+    return this.http.get(`http://localhost:8080/produtos/nome/${titulo}`);
+  }
+
+  getByTamanhoProduto(tamanho: string){
+    return this.http.get(`http://localhost:8080/produtos/nome/${tamanho}`);
+  }
+
+  getByPrecoProduto(preco: number){
+    return this.http.get(`http://localhost:8080/produtos/nome/${preco}`);
+  }
+
+  deleteById(id: number){
+    return this.http.delete(`http://localhost:8080/produtos/${id}`);
   }
 }
