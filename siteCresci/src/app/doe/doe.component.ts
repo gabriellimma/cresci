47ee8b2c93b47e717213181deedd7ea6307857cb
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-06-22 16:07:04 -0300

    trabalhando

--- siteCresci/src/app/doe/doe.component.ts
+++ siteCresci/src/app/doe/doe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Doacao } from '../model/Doacao';
+import { DoacaoService } from '../service/doacao.service';
 
 @Component({
   selector: 'app-doe',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoeComponent implements OnInit {
 
-  constructor() { }
+  listaDoacoes: Doacao[]
+
+ doacao: Doacao = new Doacao;
+
+  constructor(private doacaoService : DoacaoService) { }
 
   ngOnInit(): void{
-    window.scroll(0,0)
+    this.findAllDoacao();
+    window.scroll(0,0);
+  }
+
+  findAllDoacao(){
+    this.doacaoService.getAllDoacao().subscribe((resp: Doacao[])=>{
+      this.listaDoacoes = resp
+    })
   }
 
 }
---
+++ siteCresci/src/app/model/Doacao.ts
@@ -0,0 +1,9 @@
+export class Doacao {
+
+	public idDoacao:number;
+	public email: string;
+	public nome: string;
+	public instituicao: string;
+	public item: string;
+
+}
