7ad9c864cc6c564bae0134596ec57929395d11d1
GeisianeMaia <geisiane.maia@gmail.com>
2020-06-25 23:53:37 -0300

    put e delete no service produto

--- siteCresci/src/app/editar-meusprodutos/editar-meusprodutos.component.ts
+++ siteCresci/src/app/editar-meusprodutos/editar-meusprodutos.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-editar-meusprodutos',
-  templateUrl: './editar-meusprodutos.component.html',
-  styleUrls: ['./editar-meusprodutos.component.css']
-})
-export class EditarMeusprodutosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+        import { Produto } from '../model/Produto';
+        import { ProdutoService } from '../service/produto.service';
+        import { ActivatedRoute, Router } from '@angular/router';
+        
+        @Component({
+          selector: 'app-editar-meusprodutos',
+          templateUrl: './editar-meusprodutos.component.html',
+          styleUrls: ['./editar-meusprodutos.component.css']
+        })
+        export class EditarMeusprodutosComponent implements OnInit {
+        
+          produto:Produto = new Produto;
+          constructor(private produtoService: ProdutoService, private route: ActivatedRoute, private router: Router) { }
+        
+          ngOnInit(): void {
+            let id = this.route.snapshot.params['id']
+            this.findById(id)
+          }
+        
+          findById(id:number){
+            this.produtoService.getByIdProduto(id).subscribe((resp: Produto)=>{
+              this.produto = resp
+            })
+          }
+        
+          salvar(){
+            this.produtoService.putProduto(this.produto).subscribe((resp: Produto)=>{
+              this.produto = resp
+              this.router.navigate(['/meusprodutos'])
+              location.assign('/meusprodutos')
+            })
+          }
+        
+        
+        }
+        
--- siteCresci/src/app/service/produto.service.ts
+++ siteCresci/src/app/service/produto.service.ts
@@ -21,6 +21,10 @@ export class ProdutoService {
     return this.http.post('http://localhost:8080/produtos/cadastrar', produto, this.token);
   }
 
+  putProduto(produto: Produto) {
+    return this.http.post('http://localhost:8080/produtos/cadastrar', produto, this.token);
+  }
+
   getByIdProduto(id: number){
     return this.http.get(`http://localhost:8080/produtos/${id}`, this.token);
   }
