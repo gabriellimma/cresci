14f8bada93861a7df9c50b52efb9141ff0c38879
Bielzin42 <nickspoot@gmail.com>
2020-06-29 12:33:30 -0300

    finalmente finalizando o backend de doe

--- siteCresci/src/app/contato/contato.component.ts
+++ siteCresci/src/app/contato/contato.component.ts
@@ -9,46 +9,20 @@ import { ContatoService } from '../service/contato.service';
 })
 export class ContatoComponent implements OnInit {
 
+  listaContatos: Contato[]
   Contato: Contato = new Contato;
 
   constructor(private ContatoService : ContatoService) { }
 
   ngOnInit(): any{
-    window.scroll(0,0),
+    window.scroll(0,0)
     
-    (function() {
-      "use strict";
-      window.addEventListener(
-        "load",
-        function() {
-          // Fetch all the forms we want to apply custom Bootstrap validation styles to
-          var forms = document.getElementsByClassName("needs-validation");
-          // Loop over them and prevent submission
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener(
-              "submit",
-              function(event) {
-                if (form.checkValidity() === false) {
-                  event.preventDefault();
-                  event.stopPropagation();
-                }
-                form.classList.add("was-validated");
-              },
-              false
-            );
-          });
-        },
-        false
-      );
-    })();
+    
   }
-
   contatar() {
     this.ContatoService.postDoacao(this.Contato).subscribe((resp: Contato) => {
       this.Contato = resp
-      console.log(`Produto cadastrado com sucesso!`)
-      location.assign('/lojinha')
-      
-    }, err =>{alert(`Ops! parece que você preencheu algo errado!`), console.log(err)})
+      location.assign('/contato')      
+    })
   }
 }
