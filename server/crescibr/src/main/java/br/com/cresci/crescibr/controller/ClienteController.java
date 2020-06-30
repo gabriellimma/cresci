301021801465b6161cd3b5da07aa8fda6d77d9d1
Bielzin42 <nickspoot@gmail.com>
2020-06-30 18:29:13 -0300

    implkementando deletar usuario no backend

--- server/crescibr/src/main/java/br/com/cresci/crescibr/controller/ClienteController.java
+++ server/crescibr/src/main/java/br/com/cresci/crescibr/controller/ClienteController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,5 +56,10 @@ public class ClienteController {
 	@PutMapping("/editar")
 	public ResponseEntity<ClienteModel> Alterar(@RequestBody ClienteModel usuario){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
