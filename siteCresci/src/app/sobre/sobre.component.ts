fe36ed4b56e79e898f4f8905328518f4dbed98d3
Erick005 <erickportugaldesenvolvedor@gmail.com>
2020-06-29 22:27:36 -0300

    cultura da empresa modificada cards mudados layout mudado

--- siteCresci/src/app/sobre/sobre.component.ts
+++ siteCresci/src/app/sobre/sobre.component.ts
@@ -15,11 +15,6 @@ export class SobreComponent implements OnInit {
 
   ngOnInit(): void {
     window.scroll(0,0)
-    $('button').click(function(){
-      $('button').toggleClass('active');
-      $('.title').toggleClass('active');
-      $('nav').toggleClass('active');
-    });
     AOS.init({
       offset: 400, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
