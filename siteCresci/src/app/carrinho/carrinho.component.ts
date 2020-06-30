88c90cad35cbeffb8065aa783e5b70e00f18f9df
GeisianeMaia <geisiane.maia@gmail.com>
2020-06-30 19:19:15 -0300

    teste

--- siteCresci/src/app/carrinho/carrinho.component.ts
+++ siteCresci/src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Produto } from '../model/Produto';
+import { ProdutoService } from '../service/produto.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-carrinho',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoComponent implements OnInit {
 
-  constructor() { }
+  produto:Produto = new Produto;
+
+  constructor(private produtoService: ProdutoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+
+ 
+    window.scroll(0,0)
+    let id:number = this.route.snapshot.params['idProduto']
+    this.findById(id)
   }
 
+  
+  findById(id:number){
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto)=>{
+      this.produto = resp
+    }, err =>{
+      console.log(`Erro: ${err.status}, não conseguimos pegar o id`)
+    })
+  }
 }
