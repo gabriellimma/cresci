baa69bd29f40cdc09c43cea6263ffbbf5cc0155b
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-06-30 22:36:03 -0300

    Colocando foto do perfil ao logar - app.properties

--- server/crescibr/src/main/java/br/com/cresci/crescibr/model/ClienteLoginModel.java
+++ server/crescibr/src/main/java/br/com/cresci/crescibr/model/ClienteLoginModel.java
@@ -15,6 +15,15 @@ public class ClienteLoginModel {
 	
 	private String token;
 	
+	private String foto;	
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
 
 	public long getIdCliente() {
 		return idCliente;
